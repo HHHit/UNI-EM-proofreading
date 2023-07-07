014c197eb65251cf215440e2547f057134010995
HHHIT <boyu93@vt.edu>
2023-07-07 14:50:46 -0400

    create install .sh

---
+++ UNI_EM_Proofreading_Install.sh
@@ -0,0 +1,5 @@
+conda create -n Dojo_env python=3.6
+conda activate Dojo_env
+git clone https://github.com/HHHit/UNI-EM-proofreading.git
+cd UNI-EM-proofreading/
+pip install -r uni_em_requirements_cbil_server.txt
